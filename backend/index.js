6bf9a8535deefbe6287a03edbaa2c21636ceb177
Oliver <oliverjgodoy@gmail.com>
2020-04-24 23:37:57 -0300

    recurso

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,15 @@ const express = require('express'); //importa as funcionalidades do express
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.send('Hello World')
-})
+//
+app.post('/users', (request, response) => {
+    return response.json({
+        chave: 'valor',
+        nome: 'Ciata',
+        idade: '23',
+        classe: 'S5'
+    });
+});
 
 app.listen(3333)
 
