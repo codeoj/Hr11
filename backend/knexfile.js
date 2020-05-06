47ae97a1e5a301badea0422c689179041cb385b1
Oliver <oliverjgodoy@gmail.com>
2020-05-06 16:53:06 -0300

    sqlite

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,7 +5,10 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './src/database/db.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
     }
   },
 
